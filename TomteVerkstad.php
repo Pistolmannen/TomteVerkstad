acbf2ecdd5d5afa77ac451cd4ea3370aae59c994
Pistolmannen <erikgustafsson@gmail.com>
2024-09-23 11:52:40 +0200

    end of day

--- TomteVerkstad.php
+++ TomteVerkstad.php
@@ -8,15 +8,43 @@
 </head>
 <body>
     <?php
-
     $pdo = new PDO("mysql:dbname=TomteVerkstad;host=localhost", "dbkonstruktion", "Skata#23");
 
-    foreach( $pdo->query("select * from Tomtenisse") as $row) {
-        echo("<pre>");
-        print_r($row);
-        echo("<pre>");
+    $namn = "David";
+
+    if (empty($_POST["name"])) {
+        $name = "David";
+    } 
+    else {
+        $name = test_input($_POST["name"]);
+        echo($name);
     }
 
     ?>
+
+    <input type="text" name="name" value="name">
+    <input type="submit" name="submit" value="Submit"> 
+
+    <?php
+    echo "<br>";
+
+    $Tomtenissar = $pdo->prepare("select * from Tomtenisse where Namn = David");
+    //$Tomtenissar = $pdo->query("select * from Tomtenisse");
+    $data = $Tomtenissar->fetchAll();
+
+
+    if ($data->countRow() > 0){
+        foreach($Tomtenissar as $row) {
+            echo("<pre>");
+            print_r($row);
+            echo("<pre>");
+        }
+    }
+    else{
+        echo("no info found");
+    }
+        
+    ?>
+    
 </body>
 </html>
