dff3e157c6ad56a56880a3ba2ca689ad8564aa0b
Pistolmannen <erikgustafsson@gmail.com>
2024-10-02 10:10:50 +0200

    form for making chef is done

--- TomteVerkstad.php
+++ TomteVerkstad.php
@@ -8,7 +8,26 @@
 </head>
 <body>
     <?php
-    $pdo = new PDO("mysql:dbname=TomteVerkstad;host=localhost", "dbkonstruktion", "Skata#23");
+    $pdo = new PDO("mysql:dbname=TomteVerkstad;host=localhost", "dbkonstruktion", "Skata#23"); // koden för att ansluta till databasen som root
+
+    /*-----------------------------*\
+    |   koden för drop down menyn   | 
+    \*-----------------------------*/
+    $Dropdownissar = $pdo->prepare("select * from Tomtenisse");
+    $Dropdownissar->execute();
+    echo "<p> Name of all tomtenissar </p>";
+
+    echo "<select name = 'dropdown'>";
+    foreach($Dropdownissar as $row) {
+        echo("<option>" . $row["Namn"] . "</option>");
+    }
+    echo "</select>";
+
+    echo "<br>";
+
+    /*-------------------------------------*\
+    |   koden för att söka på tomtenissar   | 
+    \*-------------------------------------*/
 
     if (empty($_POST["name"])) {
         $name = "Name";
@@ -20,7 +39,7 @@
     ?>
 
     <form action = "TomteVerkstad.php" method = "POST">
-        Search for Tomtenissar <br>
+        <p> Search for Tomtenissar </p>
         <input type="text" name="name" value= <?php echo $name ?>>
         <input type="submit" name="submit" value="Submit"> 
     </form>
@@ -46,12 +65,15 @@
 
     echo "<br>";
     echo "<br>";
-        
+
+    /*-----------------------------------*\
+    |   koden för att skapa tomtenissar   | 
+    \*-----------------------------------*/
     ?>
 
     <form action = "TomteVerkstad.php" method = "POST">
         Create tomtenisse <br>
-        name of tomtenisse 
+        Name of tomtenisse 
         <input type="text" name="Cname" value=""> <br>
         ID of tomtenisse 
         <input type="text" name="CId" value=""> <br>
@@ -92,8 +114,64 @@
         }
     }
 
-    
+    echo "<br>";
+    echo "<br>";
 
+    /*----------------------------------------------*\
+    |   koden för att göra tomtenissar till chefer   | 
+    \*----------------------------------------------*/
+    ?>
+
+    <form action = "TomteVerkstad.php" method = "POST">
+        Make chefnisse <br>
+        Name of tomtenisse 
+        <input type="text" name="Chefname" value=""> <br>
+        ID of tomtenisse 
+        <input type="text" name="ChefId" value=""> <br>
+        Shoe size <br>
+        <input type="radio" id="none" name="Shoesize" value=""> 
+        <label for="none">No</label> <br>
+        <input type="radio" id="mini" name="Shoesize" value="mini">
+        <label for="mini">Mini</label> <br>
+        <input type="radio" id="medium" name="Shoesize" value="medium"> 
+        <label for="medium">Medium</label> <br>
+        <input type="radio" id="maxi" name="Shoesize" value="maxi"> 
+        <label for="maxi">Maxi</label> <br>
+        <input type="radio" id="ultra" name="Shoesize" value="ultra"> 
+        <label for="ultra">Ultra</label> <br>
+        <input type="radio" id="mega" name="Shoesize" value="mega">
+        <label for="mega">Mega</label> <br>
+        <input type="submit" name="submit" value="Submit"> 
+    </form>
+
+    <?php
+    /*
+    if (empty($_POST["Chefname"]) || empty($_POST["ChefId"]) || empty($_POST["Shoesize"])) {
+        echo("can't update when empty");
+    } 
+    else {
+        
+        $CreateTomtenissar = $pdo->prepare("insert into Tomtenisse(Namn, IdNr, Nötter, Russin) values(?, ?, ?, ?)");
+        $CreateTomtenissar->bindParam(1, $_POST["Cname"], PDO::PARAM_STR);
+        $CreateTomtenissar->bindParam(2, $_POST["CId"], PDO::PARAM_STR);
+        $CreateTomtenissar->bindParam(3, $_POST["Nuts"], PDO::PARAM_INT);               // John
+        $CreateTomtenissar->bindParam(4, $_POST["Raisin"], PDO::PARAM_INT);             //120617-0921-3-666973821
+        $CreateTomtenissar->execute();
+
+        if (($CreateTomtenissar->rowCount()) > 0){
+            echo "<br>";
+            echo("Insert successful");
+        }
+        else{
+            echo "<br>";
+            echo("Insert failed");
+            echo "<br>";
+            echo $CreateTomtenissar->errorCode();
+            echo "<br>";
+            print_r($CreateTomtenissar->errorInfo());
+        }
+    }
+    */
     ?>
     
 </body>
